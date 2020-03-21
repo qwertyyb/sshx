889b3f5859fb8422fb39a9af93b47217fc3b47dd
marchyang <marchyang@tencent.com>
2020-03-21 18:44:25 +0800

    fix: 修复在使用ssh密钥登录失败的问题

--- sshx.sh
+++ sshx.sh
@@ -3,10 +3,11 @@
  # @LastEditors: qwertyyb <qwertyyb@foxmail.com>
  # @FilePath: /undefined/Users/marchyang/bash/sshx.sh
  # @Date: 2020-03-15 08:19:07
- # @LastEditTime: 2020-03-15 18:18:33
+ # @LastEditTime: 2020-03-21 18:38:31
  ###
 #!/usr/bin/expect
 
+# 从登录参数字符串中获取密码
 proc get_password {args} {
   if { ![file exists "~/.ssh/sshxrc"] } {
     return false
@@ -23,6 +24,8 @@ proc get_password {args} {
   return false
 }
 
+# 如果配置中存在，更新配置中的密码
+# 如果配置中不存在，把参数和密码对应关系写入到配置中
 proc set_password {args password} {
   set fp [open "~/.ssh/sshxrc" r]
 
@@ -49,6 +52,7 @@ proc set_password {args password} {
   close $fp
 }
 
+# 请求用户输入密码，并返回用户输入的密码
 proc prompt_password {} {
   stty -echo
   # send_user -- "Password:"
@@ -68,31 +72,38 @@ spawn ssh $argv
 
 set try_times 0
 
+# 配置中已存在的密码
+set saved_password ""
+
+# 用户输入的密码
+set new_password ""
+
 expect {
   "*(yes/no)?" { send "yes\r"; exp_continue }
   "password:" {
     if { $try_times <= 0 } {
-      # 初次，尝试从配置中获取密码
+      # 初次，尝试从配置中获取密码, 并存储到 $saved_password、 $password 变量中
       set saved_password [get_password $argv]
-      set new_password $saved_password
       set password $saved_password
     }
     if {$password == false || $try_times > 0} {
       # 从配置中获取密码失败或者第一次登录失败时，要重新输入密码
-      # 把密码存储到$new_password
+      # 把密码存储到 $new_password 和 $password 变量
       set new_password [prompt_password]
       set password $new_password
     }
     if {$saved_password != false && $try_times <= 0} {
       send_user "find password in config file, try auto login"
     }
+
+    # 把从配置中获取的密码或者用户输入的密码
     send "$password\r"
 
     set try_times [expr $try_times + 1]
     exp_continue
   }
   "Last*:" {
-    if { $new_password != $saved_password } {
+    if { $new_password != "" } {
       # 使用新密码登录成功，询问是否记住密码，更新配置文件
       stty echo
       send_user "Remember Password (yes/no)?"
